812249a84efbab0622f8df0b32e4c1b6e1d6bb50
aashildte <aashildte@gmail.com>
2023-10-20 17:57:31 -0700

    added scripts for generating fibrosic geometries

---
+++ fibrosis/generate_2D_mesh.py
@@ -0,0 +1,105 @@
+"""
+
+Åshild Telle / University of Washington / 2023
+
+Script for removing cells from the geometry, replicating the progression of cardiac fibrois.
+
+"""
+
+import numpy as np
+import dolfin as df
+from mpi4py import MPI
+import matplotlib.pyplot as plt
+from argparse import ArgumentParser
+from random import choices
+
+from parameter_setup import setup_monitor
+
+from emimechanicalmodel import (
+    load_mesh,
+    compute_active_component,
+    EMIModel,
+)
+
+from setup_replacement_fibrosis import (
+    find_middle_cells_transversely,
+    find_middle_cells_longitudinally,
+    find_random_cells,
+    replace_cells_with_matrix,
+)
+
+parser = ArgumentParser()
+
+parser.add_argument(
+    "-s",
+    "--scale",
+    default=1,
+    type=float,
+    help="Scaling parameter for ECM stiffness",
+)
+
+parser.add_argument(
+    "-r",
+    "--remove_type",
+    default="longitudinally",
+    type=str,
+    options=["longitudinally", "transversely", "randomly"]
+    help="Remove cells along ... direction",
+)
+
+parser.add_argument(
+    "-n",
+    "--remove_num_cells",
+    default=0,
+    type=int,
+    help="How many cells to remove",
+)
+
+
+parser.add_argument(
+    "-sd",
+    "--seed",
+    default=0,
+    type=int,
+    help="Random seed (for 'random' distribution)",
+)
+
+
+pp = parser.parse_args()
+
+scale, remove_type, remove_num_cells, seed = (
+    pp.scale,
+    pp.remove_type,
+    pp.remove_num_cells,
+    pp.seed
+)
+
+
+# load mesh, subdomains
+
+num_cells_xdir = 6
+num_cells_ydir = 12
+
+mesh_file = f"meshes/demo_2D_{num_cells_xdir}_{num_cells_ydir}_cells_newmesh.h5"
+mesh, volumes = load_mesh(mesh_file)
+
+if remove_type == "longitudinally":
+    print(f"Removing {remove_num_cells} cells longitudinally")
+    remove_cells = find_middle_cells_longitudinally(mesh, volumes, num_cells_xdir, remove_num_cells, 1, seed)
+elif remove_type == "transversely":
+    print(f"Removing {remove_num_cells} cells transversely")
+    remove_cells = find_middle_cells_transversely(mesh, volumes, num_cells_ydir, remove_num_cells, 1, seed)
+elif remove_type == "randomly":
+    print(f"Removing {remove_num_cells} cells randomly")
+    remove_cells = find_random_cells(mesh, volumes, remove_num_cells, seed)
+else:
+    raise NotImplementedError
+
+if len(remove_cells) > 0:
+    replace_cells_with_matrix(mesh, volumes, remove_cells)
+
+fout = f"meshes/mesh_{remove_type}_{remove_num_cells}_{seed}.h5"
+
+with df.HDF5File(mesh.mpi_comm(), fout, 'w') as out:
+    out.write(mesh, 'mesh/')
+    out.write(volumes, 'subdomains/')
---
+++ fibrosis/setup_replacement_fibrosis.py
@@ -0,0 +1,122 @@
+"""
+
+Åshild Telle / University of Washington / 2023
+
+Supporting functions for the main script for generating fibrotic geometries.
+
+"""
+
+import numpy as np
+import dolfin as df
+from mpi4py import MPI
+import matplotlib.pyplot as plt
+from argparse import ArgumentParser
+import random
+
+def find_middle_cells_transversely(mesh, volumes, num_cells_transverse, num_cells_to_remove, pad, seed=None):
+    coords = mesh.coordinates()[:]
+
+    cmax = max(coords[:,0])
+    cmin = min(coords[:,0])
+
+    dx = (cmax - cmin)/num_cells_transverse
+
+    x_start = cmin + (num_cells_transverse // 2 - pad)*dx
+    x_stop = cmin + (num_cells_transverse // 2 + pad)*dx
+    ids = set()
+
+
+    for subdomain_id, node_ids in zip(volumes.array()[:], mesh.cells()[:]):
+        for node_id in node_ids:
+            if x_start < coords[node_id][0] < x_stop:
+                ids.add(subdomain_id)
+                break
+
+    if 0 in ids:
+        ids.remove(0)
+    
+    # then remove cells 
+
+    if seed is not None:
+        random.seed(seed)
+
+    remove_ids = random.sample(list(ids), k=num_cells_to_remove)
+    remove_ids.sort()
+    print(remove_ids)
+    
+    return remove_ids
+
+
+def find_middle_cells_longitudinally(mesh, volumes, num_cells_longitudinal, num_cells_to_remove, pad, seed=None):
+    coords = mesh.coordinates()[:]
+
+    cmax = max(coords[:,1])
+    cmin = min(coords[:,1])
+
+    dx = (cmax - cmin)/num_cells_longitudinal
+
+    x_start = cmin + (num_cells_longitudinal // 2 - pad)*dx
+    x_stop = cmin + (num_cells_longitudinal // 2 + pad)*dx
+    print(pad) 
+    print(cmax, cmin, dx, x_start, x_stop)
+
+    ids = set()
+    
+    for subdomain_id, node_ids in zip(volumes.array()[:], mesh.cells()[:]):
+        for node_id in node_ids:
+            if x_start < coords[node_id][1] < x_stop:
+                ids.add(subdomain_id)
+                break
+
+    if 0 in ids:
+        ids.remove(0)
+
+    if seed is not None:
+        random.seed(seed)
+
+    remove_ids = random.sample(list(ids), k=num_cells_to_remove)
+    remove_ids.sort()
+    print(remove_ids)
+    
+    return remove_ids
+
+
+def find_random_cells(mesh, volumes, num_cells, seed=None):
+    if seed is not None:
+        random.seed(seed)
+
+    cells = list(set(volumes.array()[:]))
+    cells.remove(0)
+
+    ids = random.sample(cells, k=num_cells)
+    print(ids)
+    
+    return ids
+
+
+def replace_cells_with_matrix(mesh, volumes, cell_idts):
+    N = volumes.size()
+    
+    for i in range(N):
+        n = volumes.array()[i]
+        if n in cell_idts:
+            volumes.array()[i] = 0
+
+
+def generate_fibrotic_tissue(mesh, volumes, cell_idts):
+    # convention: 0 = fibrotic, 1 = healthy
+
+    print(cell_idts)
+    
+    volumes_array = volumes.array()[:]
+    new_volumes = df.MeshFunction("size_t", mesh, mesh.topology().dim(), 0)
+    matrix_idt = 0
+
+    volumes_array = np.where(volumes_array == 0, -1, volumes_array) # hack
+
+    for cell_idt in cell_idts:
+        new_array = np.where(volumes_array == cell_idt, 0, volumes_array)
+        volumes_array = new_array
+
+    new_volumes.array()[:] = np.where(volumes_array == 0, 0, 1)
+    return new_volumes
